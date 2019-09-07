b4378614c2555a300298607521e36dd549384997
crycetruly <crycetruly@gmail.com>
2019-09-07 16:33:23 +0300

    ft(auth): Verify user Accounts

--- instagram/apps/authentication/urls.py
+++ instagram/apps/authentication/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register', views.RegistrationView.as_view(), name='register'),
-    path('login', views.LoginView.as_view(), name='login')
+    path('login', views.LoginView.as_view(), name='login'),
+    path('activate/<uidb64>/<token>', views.VerificationView.as_view(), name='activate'),
+    path('profile',views.ProfileView.as_view(),name='profile')
 ]
--- instagram/settings.py
+++ instagram/settings.py
@@ -135,6 +135,12 @@ MESSAGE_TAGS = {
 }
 
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'landvilleng@gmail.com'
+EMAIL_HOST_PASSWORD = '@landvilleng.'
+EMAIL_PORT = 587
+
 django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
